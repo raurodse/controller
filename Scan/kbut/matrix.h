940d119abc39c413bfa0dc089388e677b7b0fd4d
raurodse <raurodse@gmail.com>
2016-10-12 22:52:11 +0200

    New matrix definition

--- Scan/kbut/matrix.h
+++ Scan/kbut/matrix.h
@@ -48,8 +48,8 @@
 //  PTD1..7
 
 // Define Rows (Sense) and Columns (Strobes)
-GPIO_Pin Matrix_cols[] = { gpio(B,16), gpio(B,17), gpio(D,0), gpio(A,12), gpio(A,13), gpio(D,7), gpio(D,4), gpio(D,2),gpio(D,3),gpio(C,3),gpio(C,4),gpio(C,6),gpio(C,7),gpio(A,5),gpio(D,1),gpio(C,0)};
-GPIO_Pin Matrix_rows[] = { gpio(B,0),gpio(B,1), gpio(B,3), gpio(B,2), gpio(D,5), gpio(D,6), gpio(C,1), gpio(C,2) };
+GPIO_Pin Matrix_cols[] = { gpio(B,16), gpio(B,17), gpio(D,0), gpio(A,12), gpio(A,13), gpio(D,7), gpio(D,4), gpio(D,2),gpio(B,0),gpio(B,1), gpio(B,3), gpio(B,2), gpio(D,5), gpio(D,6), gpio(C,1), gpio(C,2)};
+GPIO_Pin Matrix_rows[] = { gpio(D,3),gpio(C,3),gpio(C,4),gpio(C,6),gpio(C,7),gpio(A,5),gpio(D,1),gpio(C,0)};
 
 // Define type of scan matrix
 Config Matrix_type = Config_Pulldown;
